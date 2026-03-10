cf9e3b0f2c3f4c374302daff376e8470f60f901a
Kyle McLeod <kylemcleod1@gmail.com>
2026-03-10 14:58:15 -0700

    add ganalytics

--- bpl-website/src/App.jsx
+++ bpl-website/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Facility from './pages/Facility'
@@ -7,6 +8,16 @@ import Founders from './pages/Founders'
 import GetInvolved from './pages/GetInvolved'
 
 export default function App() {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('config', 'G-L9MWC7N8JM', {
+        page_path: location.pathname + location.search,
+      })
+    }
+  }, [location])
+
   return (
     <Routes>
       <Route element={<Layout />}>
